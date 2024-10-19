acc167d2eb9735308d208def61f318393da3e4de
Patrik Stutz <patrik.stutz@gmail.com>
2024-10-19 11:46:27 +0200

    make all versions work on linux

--- flake.nix
+++ flake.nix
@@ -1,9 +1,15 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs-openssl_1_1.url = "github:NixOS/nixpkgs/30d3d79b7d3607d56546dd2a6b49e156ba0ec634";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      nixpkgs-openssl_1_1,
+      ...
+    }:
     let
       releases = builtins.fromJSON (builtins.readFile ./releases.json);
     in
@@ -24,18 +30,35 @@
         }:
         with pkgs;
         let
+          pkgs-openssl_1_1 = import nixpkgs-openssl_1_1 {
+            inherit system;
+            config = {
+              permittedInsecurePackages = [
+                "openssl-1.0.2u"
+              ];
+            };
+          };
           availableReleases = lib.attrsets.filterAttrs (
-            version: systems: builtins.hasAttr system systems
+            version: release: builtins.hasAttr system release.platforms
           ) releases;
           versions = (
             builtins.mapAttrs (
               version: systems:
               let
-                variant = builtins.elemAt availableReleases.${version}.${system} 0;
+                release = availableReleases.${version};
+                nixpkgsVersion = if release.openssl == "3.0" then pkgs else pkgs-openssl_1_1;
+                variant = builtins.elemAt release.platforms.${system} 0;
               in
-              callPackage ./mongodb.nix {
+              nixpkgsVersion.callPackage ./mongodb.nix {
                 inherit version;
                 inherit (variant) url sha256;
+                openssl =
+                  if release.openssl == "1.0" then
+                    nixpkgsVersion.openssl_1_0_2
+                  else if release.openssl == "1.1" then
+                    nixpkgsVersion.openssl_1_1
+                  else
+                    nixpkgsVersion.openssl;
               }
             ) availableReleases
           );
@@ -52,7 +75,7 @@
               name = "update-releases";
               runtimeInputs = [ bun ];
               text = ''
-                bun ./update-releases.sh
+                bun ./update-releases.ts
               '';
             };
           } // versions;
--- update-releases.ts
+++ update-releases.ts
@@ -5,6 +5,7 @@ const versions = (await (
 ).json()) as {
   versions: Array<{
     version: string;
+    date: string;
     downloads: Array<{
       arch: string;
       target: string;
@@ -17,7 +18,12 @@ const versions = (await (
   }>;
 };
 
-type Releases = Record<string, Platforms>;
+type Releases = Record<string, Release>;
+
+type Release = {
+  openssl: "1.0" | "1.1" | "3.0";
+  platforms: Platforms;
+};
 
 type Platforms = Record<string, Variant[]>;
 
@@ -29,40 +35,56 @@ type Variant = {
 const architectures = ["x86_64", "arm64"];
 const platforms = ["darwin", "linux"];
 
+const SWITCH_TO_OPENSSL_3 = new Date("2022-01-01");
+const SWITCH_TO_OPENSSL_1_1 = new Date("2018-08-01");
+
 const releases: Releases = Object.fromEntries(
   versions.versions
     .filter((version) => !version.version.includes("-"))
-    .map((version) => [
-      version.version.replaceAll(".", "-"),
-      Object.fromEntries(
-        architectures.flatMap((arch) =>
-          platforms.flatMap((platform) => {
-            const variants = version.downloads.filter(
-              (download) =>
-                (arch === "x86_64"
-                  ? arch === download.arch
-                  : download.arch === "arm64" || download.arch === "aarch64") &&
-                (platform === "darwin"
-                  ? download.target === "macos"
-                  : download.target.startsWith("ubuntu")) &&
-                download.edition !== "enterprise"
-            );
-            if (!variants.length) return [];
-            return [
-              [
-                `${arch}-${platform}`,
-                variants
-                  .map((download) => ({
-                    url: download!.archive.url,
-                    sha256: download!.archive.sha256,
-                  }))
-                  .slice(0, 1),
-              ],
-            ];
-          })
-        )
-      ),
-    ])
+    .map((version) => {
+      const date = new Date(version.date);
+      return [
+        version.version.replaceAll(".", "-"),
+        {
+          openssl:
+            date >= SWITCH_TO_OPENSSL_3
+              ? "3.0"
+              : date >= SWITCH_TO_OPENSSL_1_1
+              ? "1.1"
+              : "1.0",
+          platforms: Object.fromEntries(
+            architectures.flatMap((arch) =>
+              platforms.flatMap((platform) => {
+                const variants = version.downloads.filter(
+                  (download) =>
+                    (arch === "x86_64"
+                      ? arch === download.arch
+                      : download.arch === "arm64" ||
+                        download.arch === "aarch64") &&
+                    (platform === "darwin"
+                      ? download.target === "macos"
+                      : download.target.startsWith("ubuntu")) &&
+                    download.edition !== "enterprise"
+                );
+                if (!variants.length) return [];
+                return [
+                  [
+                    `${arch}-${platform}`,
+                    variants
+                      .toSorted((a, b) => b.target.localeCompare(a.target))
+                      .map((download) => ({
+                        url: download!.archive.url,
+                        sha256: download!.archive.sha256,
+                      }))
+                      .slice(0, 1),
+                  ],
+                ];
+              })
+            )
+          ),
+        },
+      ];
+    })
 );
 
 writeFileSync("./releases.json", JSON.stringify(releases, null, "\t"));
